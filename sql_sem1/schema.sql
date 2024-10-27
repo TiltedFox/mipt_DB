f987ccc33622e5acc3a6bd8b7341998eae5e1658
KrasikovViktor <vitya22898@gmail.com>
2024-10-27 21:47:42 +0300

    Made project homework

---
+++ sql_sem1/schema.sql
@@ -0,0 +1,11 @@
+CREATE table devices (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	sn TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE tests (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	ts TEXT NOT NULL,
+	device_id INTEGER NOT NULL REFERENCES devices(id),
+	result INTEGER --0 - fail, 1 - passed
+);
